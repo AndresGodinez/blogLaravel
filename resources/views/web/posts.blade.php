7cbabb67db5171486582b1145d35b6b20d0503d2
Andres Vagrant <ing.a.godinez@gmail.com>
2018-01-27 23:08:23 +0000

    vista de post

---
+++ resources/views/web/posts.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+	<div class="container">
+		<div class="col-md-8 col-md-offset-2">
+			<h1>Lista de Post</h1>
+			
+			@foreach ($posts as $post)
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						{{$post->name}}
+					</div>
+					<div class="panel-body">
+						@if ($post->file)
+							<img src="{{$post->file }}" class="img-responsive">
+						@endif
+						{{$post->excerpt}}
+						<a href=" {{ route('post', $post->slug ) }} " class="btn btn-primary pull-right">Leer Más</a>
+					</div>
+				</div>
+
+			@endforeach
+
+			{{ $posts->render() }}
+		</div>
+	</div>
+@endsection
