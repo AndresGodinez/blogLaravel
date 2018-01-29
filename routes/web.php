38a65117d174a655a84ea8a3eab90084ac53f9e9
Andres Vagrant <ing.a.godinez@gmail.com>
2018-01-29 01:18:41 +0000

    cambio basico de estructura
    Se crean los directorios para separar las estructuras de las demas carpetas
    se organiza todo en base a si es de area web o es de area de administración

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ Auth::routes();
 
 Route::get('blog', 'Web\PageController@blog')->name('blog');
 
+//Web
 Route::get('entrada/{slug}', 	'Web\PageController@post')->name('post');
 Route::get('categoria/{slug}', 	'Web\PageController@category')->name('category');
 Route::get('etiqueta/{slug}', 	'Web\PageController@tag')->name('tag');
+
+
+// Admin
+Route::resource('tags', 		'Admin\TagController');
+Route::resource('categories', 	'Admin\CategoryController');
+Route::resource('posts', 		'Admin\PostsController');
